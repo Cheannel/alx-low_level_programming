dbd9bbd772d7c7ae280c0a9c8c75248b6a650ca7
Cheannel <immaculatelughas@gmail.com>
2022-09-11 18:45:37 -0700

    C-variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = '0'; x <= '7'; x++)
+	for (x = '0'; x < 8; x++)
 	{
-		for (y = '1'; y <= '8'; y++)
+		for (y = 1; y < 9; y++)
 		{
-			for (z = '2'; z <= '9'; z++)
+			for (z = 2; z < 10; z++)
 			{
 				putchar((x % 10) + '0');
 			putchar((y % 10) + '0');

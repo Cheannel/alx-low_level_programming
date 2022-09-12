ebada8f873712486f9892dfcbe9f6f83657a3b1a
Cheannel <immaculatelughas@gmail.com>
2022-09-12 04:25:44 -0700

    Printing hexadecimal numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,9 @@ int main(void)
 	int x, y;
 
 	for (x = 0; x <= 9; x++)
-	{
-		putchar(x);
+		putchar((x % 10) + '0');
 		for (y = 'a'; y <= 'f'; y++)
 			putchar(y);
-	}
 	putchar('\n');
 	return (0);
 }

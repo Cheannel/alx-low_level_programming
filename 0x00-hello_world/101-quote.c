2a0fe9a9938a5aa7e5458a0dc9b4bec9061edc9d
Cheannel <immaculatelughas@gmail.com>
2022-09-12 21:33:39 -0700

    Low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main  = main block
- * Return: 1
+ * main  = prints "and that piece of art is uaeful" - Dora Korpar, 2015-10-19
+ * Return: Always 0
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
+	write(2,
+			"and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}

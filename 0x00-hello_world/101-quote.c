256d3789385fac68a24a1fe81bfcbc5a921da57e
Cheannel <immaculatelughas@gmail.com>
2022-09-12 22:02:03 -0700

    Updatef

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main  = prints "and that piece of art is uaeful" - Dora Korpar, 2015-10-19
- * Return: Always 0
+ * main  = prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Description: printing an advice
+ * Return: always 0
  */
 int main(void)
 {
 
-	write(2,
-			"and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}

1b86fd7e4f902c035e1941a804423a973ceb8c7d
Cheannel <immaculatelughas@gmail.com>
2022-09-12 19:46:01 -0700

    Printing variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description: printing all combinations oftwo 2-digit numbers
+ * Description: printing combinations of two 2-digit numbers
  * Return: always 0
  */
 int main(void)
 {
-int x, y;
+	int x, y;
 
-for (x = 0; x < 99; x++)
-{
-	for (y = 1; y <= 99; y++)
+	for (x = 0; x <= 98; x++)
 	{
-		putchar((x / 10) + '0');
-		putchar((x % 10) + '0');
-		putchar(' ');
-		putchar((y / 10) + '0');
-		putchar((y % 10) + '0');
-		if (x == 98 && y == 99)
-			continue;
-		putchar(',');
-		putchar(' ');
+		for (y = 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 98 && y == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
+		}
 	}
+
+	putchar('\n');
+	return (0);
 }
-putchar('\n');
-return (0);
-}
+
 

1db19560a76b645474cd8c053dfa5067dd0e3c7f
Cheannel <immaculatelughas@gmail.com>
2022-09-13 19:37:19 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: printing underscore putchar(_putchar)
+ * Return: always 0
+ */
+int main(void)
+{
+	int _putchar;
+
+	putchar(_putchar);
+
+	putchar('\n');
+	return (0);
+}
+

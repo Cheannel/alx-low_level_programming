7b2c11138e95bb60f7b30575b7bea8dea6c06b6e
Cheannel <immaculatelughas@gmail.com>
2022-09-13 21:56:36 -0700

    Task updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
  * file - main.h
  * Description: the prototypes of all the functions used in this project (0x02-functions_nested_loops) 
  */
-int _putchar(char c)
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);

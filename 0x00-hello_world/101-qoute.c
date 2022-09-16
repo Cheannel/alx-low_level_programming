802777e7a8b87760a6529ebec3b2f2638792fb6d
Cheannel <immaculatelughas@gmail.com>
2022-09-16 09:39:13 -0700

    Printing a quote

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
- * main - main block
+ * main - entry point
  * Description: prints an advice
  * Return: always 1
  */
 int main(void)
 {
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main  = prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
  * Description: printing an advice
- * Return: always 0
+ * Return: always 1
  */
 int main(void)
 {
 
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }

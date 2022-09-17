3058fff3c1ad0cefc3ef8f3c0d9c2a509ec34c39
Cheannel <immaculatelughas@gmail.com>
2022-09-16 19:22:53 -0700

    Test function whether positive or negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,10 +8,10 @@
 
 
 
-void positive_or_negative(int n)
-int largest_numbers(int a, int b, int c)
-int convert_day(int month, int day)
-void print_remaining_days(int month, int day, int year)
+void positive_or_negative(int n);
+int largest_numbers(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 
 

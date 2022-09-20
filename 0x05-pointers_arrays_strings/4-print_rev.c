3dc8727dac8f31f373ca3d14f0e76db1adbc96d6
Cheannel <immaculatelughas@gmail.com>
2022-09-19 22:30:18 -0700

    Printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ * description: prints a string in reverse
+ * @s: integer value
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, rev;
+
+	while (s[rev++])
+		len++;
+	for (len = rev - 1; rev >= 0; rev--)
+		_putchar(s[rev]);
+	_putchar('\n');
+}

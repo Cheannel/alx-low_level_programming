3b0c06b09e36922c0cc5f4e0862fefd7c6ad089a
Cheannel <immaculatelughas@gmail.com>
2022-09-20 20:23:12 -0700

    Task done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  *Return: the value of converted string
  */
 int _atoi(char *s)
-{	
+{
 	int sign = 1;
 	unsigned int num  = 0;
 
@@ -14,9 +14,10 @@ int _atoi(char *s)
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num (num * 10) + (*s - '0');
-		elae if (num > 0)
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
 			break;
 	} while (*s++);
+
 	return (num * sign);
 }

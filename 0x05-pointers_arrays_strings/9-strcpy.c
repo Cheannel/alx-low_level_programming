fd8d094c98afdeca2bc0f09cdc1e5620b08d8275
Cheannel <immaculatelughas@gmail.com>
2022-09-20 19:32:22 -0700

    Copying a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - entry point
+ * description: a function that copies a string
+ *@dest: the buffer
+ *@src: string source
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int z = 0;
+
+	while (src[z])
+	{
+		dest[z] = src[z];
+		z++;
+	}
+	return (dest);
+}

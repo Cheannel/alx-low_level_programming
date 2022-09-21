2c61660c11b090ecd9646390e27cf5377f4d233e
Cheannel <immaculatelughas@gmail.com>
2022-09-20 20:13:34 -0700

    Task done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,8 +3,20 @@
  * _atoi - entry point
  * description: a function that converts a string to an integer
  *@s: the string
- *Return: always 0
+ *Return: the value of converted string
  */
 int _atoi(char *s)
-{
-	
+{	
+	int sign = 1;
+	unsigned int num  = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num (num * 10) + (*s - '0');
+		elae if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
+}

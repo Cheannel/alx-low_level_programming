8054e27d076357cee6faafbb5338fcf4c5d7eac5
Cheannel <immaculatelughas@gmail.com>
2022-09-22 20:34:58 -0700

    Capitalizing all the words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,16 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
 		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
 		|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
 		|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
 		|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-		|| str[i - 1] == '}')
+		|| str[i - 1] == '}' || i == 0)
+			str[i] -= 32;
 
 		i++;
 	}

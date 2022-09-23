8318cb9f3bf8a0b525e9049f9df4d5738201cdc3
Cheannel <immaculatelughas@gmail.com>
2022-09-23 08:11:17 -0700

    Printing an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,9 +15,9 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-	if (num == 0)
-		_putchar('0');
-	if (num / 10)
+
+	if ((num / 10) > 0)
 		print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }

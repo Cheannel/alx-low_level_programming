595deaf928c486e6535cdca19919176caec90afb
Cheannel <immaculatelughas@gmail.com>
2022-09-28 20:41:19 -0700

    Number factorials

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * factorial - entry point
+ * description: a function that returns the factorial of a given number
+ * @n: the number
+ * Return: -1 if n is lower than 0(representing an error)
+ * return factorial of n, if n > 0
+ */
+int factorial(int n)
+{
+	int i;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0 && n <= 1)
+		return (1);
+	i = factorial(n - 1);
+	return (i);
+}

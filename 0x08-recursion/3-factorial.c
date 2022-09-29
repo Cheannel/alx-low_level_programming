bff9b11935d0440727dd519af271ba893fc1bb9e
Cheannel <immaculatelughas@gmail.com>
2022-09-28 20:49:03 -0700

    Number factorials

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,12 +9,12 @@
  */
 int factorial(int n)
 {
-	int i;
+	int i = n;
 
 	if (n < 0)
 		return (-1);
 	if (n >= 0 && n <= 1)
 		return (1);
-	i = factorial(n - 1);
+	i *= factorial(n - 1);
 	return (i);
 }

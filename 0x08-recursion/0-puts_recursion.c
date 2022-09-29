3a0eb63e299afb011f390fdbfe90bd8e15f7825c
Cheannel <immaculatelughas@gmail.com>
2022-09-28 19:07:19 -0700

    Project started and task 0 added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion - entry point
+ * description: a funtion that prints a string, followed by a new line
+ * @s: the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	_putchar("*s\n");
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* filename: main.h
+* description: a header file containing all prototypes used in this
+* project(0x08-recursion)
+*/
+
+
+void _puts_recursion(char *s);
+int _putchar(char *c);
+int _sqrt_recursion(int n);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
+
+
+#endif

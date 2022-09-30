6ab8ff174bce9f090d1b1f772a1f2bdf6ac409e1
Cheannel <immaculatelughas@gmail.com>
2022-09-29 17:21:11 -0700

    Prints a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 /**
  * _puts_recursion - entry point
  * description: a funtion that prints a string, followed by a new line
@@ -9,9 +9,9 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(s);
+		putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-		_putchar("\n");
+		putchar('\n');
 }

21ac099b00d95bcd2c3588193bb48600d5182c43
Cheannel <immaculatelughas@gmail.com>
2022-10-05 22:01:49 -0700

    Task 0 updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: the array of pointers to the arguments
  * Return: always 0
  */
-int main(__attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

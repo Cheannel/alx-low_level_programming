9628fdaf4a4231a3cb681e096322d3ec10c98ca0
Cheannel <immaculatelughas@gmail.com>
2022-10-07 19:01:27 -0700

    Task 1 updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	len = 0;
 	for (i = 0; s1[i]; i++)
-		concat[len] = s1[i];
+		concat[len++] = s1[i];
 	for (i = 0; s2[i] && i < n; i++)
 		concat[len++] = s2[i];
 	concat[len] = '\0';

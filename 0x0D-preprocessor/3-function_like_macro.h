61a9026b3b73f8c440917c2564ad465cf09c5773
Cheannel <immaculatelughas@gmail.com>
2022-10-10 21:56:28 -0700

    Task 3 and 4 completed

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * filename: 3-function_like_macro.h
+ * description: a function-like macro ABS(x) that computes the absolute value
+ * of a number 'x'
+ */
+#define ABS(x) (x)
+
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * filename: 4-sum.h
+ * description:  a function-like macro SUM(x, y) that computes the sum
+ * of the numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+
+#endif

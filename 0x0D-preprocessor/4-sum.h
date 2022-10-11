8f47204151644a449f3e76ac8dea8ca70f3b265c
Cheannel <immaculatelughas@gmail.com>
2022-10-10 22:00:15 -0700

    Task 3 and 4 completed

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -5,6 +5,8 @@
  * filename: 4-sum.h
  * description:  a function-like macro SUM(x, y) that computes the sum
  * of the numbers x and y
+ * @x: first number to be added
+ * @y: second number to be added
  */
 
 #define SUM(x, y) ((x) + (y))
